4fa902f1e68ed25ff06f4e2bb7e929a25ae87fc8
willcao <willcao@umich.edu>
2021-04-29 15:16:14 -0400

    working triangle with shaders

--- main.cpp
+++ main.cpp
@@ -1,19 +1,81 @@
 #define GL_SILENCE_DEPRECATION
+#include <string>
+#include <iostream>
+#include <cassert>
 #include <GLFW/glfw3.h>
 #include <OpenGL/gl3.h>
 
-int main(void)
-{
-  GLFWwindow* window;
+static GLuint compileShader(const std::string &source, GLenum type){
+  GLuint id{glCreateShader(type)};
+  const char *src = source.c_str();
+  glShaderSource(id, 1, &src, NULL);
+  glCompileShader(id);
+
+  GLint result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if(result == GL_FALSE){
+    // error in compiling
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    // TODO: unique pointer learning?
+    char *message = new char[length];
+    glGetShaderInfoLog(id, length, &length, message);
+    std::cout << "Failed to compile shader "
+              << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "\n";
+    std::cout << message << "\n";
+    delete[] message;
+    return 0;
+  }
+
+  return id;
+}
+
+/**
+ * Create a shader program
+ * @param vertexShader Source code for vertex shader
+ * @param fragmentShader Source code for fragment shader
+ * @return
+ */
+static GLuint createShaders(const std::string &vertexShader, const std::string &fragmentShader){
+  GLuint program{glCreateProgram()};
+  GLuint vs{compileShader(vertexShader, GL_VERTEX_SHADER)};
+  GLuint fs{compileShader(fragmentShader, GL_FRAGMENT_SHADER)};
+
+  assert(vs);
+  assert(fs);
+
+  // link the shaders to the "program"
+  // like joining the .o program to main.cpp
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  // shaders are now part of "program"
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  // like deleting the .o files when compiling c code
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  // technically should run glDeleteShader
+  return program;
+}
+
+int main(void) {
+  GLFWwindow *window;
 
   /* Initialize the library */
   if (!glfwInit())
     return -1;
 
+  //https://www.glfw.org/faq.html#41---how-do-i-create-an-opengl-30-context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window)
-  {
+  if (!window) {
     glfwTerminate();
     return -1;
   }
@@ -21,17 +83,52 @@ int main(void)
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  float positions[6] = {
+    -0.5f, -0.5f, // this is 1st vertex
+    0.0f, 0.5f, // this is the 2nd vertex
+    0.5f, -0.5f // this is the 3rd vertex
+  };
+  GLuint VBO;  // id of the buffer
+  glGenBuffers(1, &VBO);  // creates a buffer, stores the ID in "buffer"
+  // OpenGL will use the buffer that is currently binded
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  // create an array buffer (byte array)
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  // must have this if we use attribute
+  glEnableVertexAttribArray(0); // 0 is the index of attribute storing position
+  // do this for each attribute of the vertex
+  // in this case we just have 1 attribute
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+
+  std::cout << "OpenGL version " << glGetString(GL_VERSION) << std::endl;
+  std::cout << "GLSL version " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+  std::string vertexShader =
+    "#version 330 core\n"
+    "layout(location = 0) in vec2 position;\n"
+    "void main(){\n"
+    "   gl_Position = vec4(position, 0.0, 1.0);\n" // openGL knows "position" is a vec2, but can convert automatically to vec4
+    "}\n";
+  std::string fragmentShader =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main(){\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
+  GLuint shader = createShaders(vertexShader, fragmentShader);
+  glUseProgram(shader);
+
   /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window))
-  {
+  while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
